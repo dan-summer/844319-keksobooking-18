3ba53112adc313f347dc23ed0b56e6b113a1d2f1
DanSummer <dan.summer363@gmail.com>
2019-09-22 19:33:41 +0300

    Доработан main.js для проверки

--- js/main.js
+++ js/main.js
@@ -35,14 +35,27 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 var LOCATION_X_MIN = 0;
-var LOCATION_X_MAX = 890;
-var LOCATION_X_MIN = 130;
+var LOCATION_X_MAX = document.querySelector('.map__overlay').offsetWidth;
+var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
+var PIN_WIDTH = 62;
+var PIN_HEIGHT = 62;
 var COUNT = 8;
+
+// Активная карта
+var activeMap = document.querySelector('.map');
+activeMap.classList.remove('map--faded');
+
+// Метки объявлений
+var mapPins = document.querySelector('.map__pins');
+
+// Шаблон метки
+var pinTamplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 // Функция создания массива чисел
 var createNumbersArray = function (count) {
   var numbers = [];
+
   for (var i = 1; i <= count; i++) {
     numbers.push(i);
   }
@@ -73,21 +86,25 @@ var getRandomArrayElement = function (arr) {
   return arr[randomIndex];
 };
 
-// Функция генерации случайного числа в диапазоне
-// var getRandomInRange = function (min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
 // Функция генерации случайного массива
 var getRandomArray = function (arr) {
-  for (var i = arr.length - 1; i > 0; i--) {
-	var j = Math.floor(Math.random() * (i + 1));
-	var temp = arr[j];
-	arr[j] = arr[i];
-	arr[i] = temp;
+  var copiedArr = arr.slice();
+  var count = getRandomNumber(arr.length);
+
+  var newArray = [];
+
+  for (var i = 0; i < count; i++) {
+    var index = getRandomNumber(copiedArr.length);
+    newArray.push(copiedArr[index]);
+    copiedArr.splice(index, 1);
   }
 
-  return arr.slice(getRandomNumber(arr.length), arr.length);
+  return newArray;
+};
+
+// Функция генерации случайного числа в диапазоне
+var getRandomInRange = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Функция создания объекта
@@ -98,20 +115,52 @@ var createAnnouncement = function () {
     },
     offer: {
       title: getRandomArrayElement(TITLES),
-      address: getRandomArrayElement(ADDRESS),
-      price: getRandomArrayElement(PRICE),
+      address: getRandomArrayElement(ADDRESSES),
+      price: getRandomArrayElement(PRICES),
       type: getRandomArrayElement(TYPES),
       rooms: getRandomArrayElement(ROOMS),
       guests: getRandomArrayElement(GUESTS),
       checkin: getRandomArrayElement(CHECK_TIMES),
       checkout: getRandomArrayElement(CHECK_TIMES),
       features: getRandomArray(FEATURES),
-      description: 'Описание',
-      photos:
+      description: DESCRIPTION,
+      photos: getRandomArray(PHOTOS)
     },
     location: {
-      x:
-      y:
+      x: getRandomInRange(LOCATION_X_MIN, LOCATION_X_MAX),
+      y: getRandomInRange(LOCATION_Y_MIN, LOCATION_Y_MAX)
     }
   };
 };
+
+// Функция добавления объявления в массив
+var createAnnouncements = function (count) {
+  var announcements = [];
+
+  for (var i = 0; i < count; i++) {
+    announcements.push(createAnnouncement());
+  }
+
+  return announcements;
+};
+
+// Функция отрисовки меток объявлений
+var renderPin = function (pin) {
+  var pinElement = pinTamplate.cloneNode(true);
+
+  pinElement.style.left = pin.location.x - PIN_WIDTH / 2 + 'px';
+  pinElement.style.top = pin.location.y - PIN_HEIGHT + 'px';
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = pin.offer.title;
+
+  return pinElement;
+};
+
+// Добавление элементов на страницу
+var fragment = document.createDocumentFragment();
+var announcements = createAnnouncements(COUNT);
+
+for (var i = 0; i < announcements.length; i++) {
+  fragment.appendChild(renderPin(announcements[i]));
+}
+mapPins.appendChild(fragment);
